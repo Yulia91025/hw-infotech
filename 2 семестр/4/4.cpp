b03b8fe5cf585ba3ac767e60083de4c79946d734
Юлия Худеньких <71973418+Yulia91025@users.noreply.github.com>
2021-06-07 23:50:02 +0300

    8.cpp

--- 2 семестр/4/4.cpp
+++ 2 семестр/4/4.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-
 class Complex {
     double x, y;
 public:
-    Complex(){
+    Complex() {
         x = 0;
         y = 0;
     }
@@ -20,37 +19,70 @@ public:
     ~Complex() {}
     void in() {
         double a, b;
-        std::cout <<endl<< "Re(z)=";
+        std::cout << endl << "Re(z)=";
         std::cin >> a;
-        std::cout <<endl<< "Im(z)=";
+        try{
+            if (a>10000000000000000000)
+                throw "Too big number!\n";
+            x = a;
+        }
+        catch (const char* e) {
+            std::cerr << e;
+        }
+        std::cout << endl << "Im(z)=";
         std::cin >> b;
-        x = a;
-        y = b;
+        try {
+            if (a > 10000000000000000000)
+                throw "Too big number!\n";
+            y = b;
+        }
+        catch (const char* e) {
+            std::cerr << e;
+        }
         std::cout << endl;
     }
     void out() {
-        if (y > 0) {if(y!=1) std::cout << x << '+' << y << "*i" <<' '; 
-                   else std::cout << x << '+' << "i" << ' ';}
-        else if (y < 0) {if (y!=-1) std::cout << x << y << "*i" << ' ';
-                         else std::cout << x << '-' << "i" << ' ';}
-        else std::cout << x <<' ';
+        if (y > 0) {
+            if (y != 1) std::cout << x << '+' << y << "*i" << ' ';
+            else std::cout << x << '+' << "i" << ' ';
+        }
+        else if (y < 0) {
+            if (y != -1) std::cout << x << y << "*i" << ' ';
+            else std::cout << x << '-' << "i" << ' ';
+        }
+        else std::cout << x << ' ';
+        std::cout << std::endl;
     }
-    Complex operator + (const Complex& A){
-        return Complex(x + A.x, y+A.y);
+    Complex operator + (const Complex& A) {
+        return Complex(x + A.x, y + A.y);
     }
-    Complex operator - (const Complex& A){
+    Complex operator - (const Complex& A) {
         return Complex(x - A.x, y - A.y);
     }
-    Complex operator * (const Complex& A){
-        return Complex(x*A.x-y*A.y, x*A.y + y*A.x);
+    Complex operator * (const Complex& A) {
+        return Complex(x * A.x - y * A.y, x * A.y + y * A.x);
     }
-    Complex operator / (const Complex& A){
-        return Complex((x * A.x + y * A.y)/(A.x*A.x+A.y*A.y), (  y * A.x- x * A.y)/ (A.x * A.x + A.y * A.y));
+    Complex operator / (const Complex& A) {
+        try {
+            if ((A.x * A.x + A.y * A.y) == 0)
+                throw "The divisor is 0!\n";
+        }
+        catch (const char* e) {
+            std::cerr << e;
+        }
+        return Complex((x * A.x + y * A.y) / (A.x * A.x + A.y * A.y), (y * A.x - x * A.y) / (A.x * A.x + A.y * A.y));
     }
     double abs() {
-        return sqrt(x*x+y*y);
+        return sqrt(x * x + y * y);
     }
     Complex conj() {
+        try {
+            if (y == 0)
+                throw "The number is real!\n";
+        }
+        catch (const char* e) {
+            std::cout << e;
+        }
         Complex C(x, -y);
         return C;
     }
@@ -58,36 +90,22 @@ public:
 };
 
 void comp(Complex A, Complex B) {
-    if (A.abs() > B.abs()) 
-            { A.out(); std::cout << ">"<<' '; B.out(); std::cout << endl;}
-             else if (A.abs() < B.abs()) 
-                     { A.out(); std::cout << "<"<<' '; B.out(); std::cout << endl;}
-                     else { A.out(); std::cout << "="<<' '; B.out(); std::cout << endl;}
+    if (A.abs() > B.abs())
+    {
+        A.out(); std::cout << ">" << ' '; B.out(); std::cout << endl;
+    }
+    else if (A.abs() < B.abs())
+    {
+        A.out(); std::cout << "<" << ' '; B.out(); std::cout << endl;
+    }
+    else { A.out(); std::cout << "=" << ' '; B.out(); std::cout << endl; }
 }
-
-int main(){
-    Complex A(0,0),C(0,0),B(0,0);
+int main() {
+    Complex A;
     A.in();
     A.out();
+    Complex B;
+    A / B;
     B.in();
-    B.out();
-    C.in();
-    C.out();
-    std::cout << endl;
-    std::cout << A.abs()<<endl;
-    A.conj().out();
-    std::cout << endl;
-    A = B + C;
-    A.out();
-    std::cout << endl;
-    A= B-A;
-    A.out();
-    std::cout << endl;
-    B = B * C;
-    B.out();
-    std::cout << endl;
-    B = B / C;
-    B.out();
-    std::cout << endl;
-    comp(A, B);
+    B.conj();
 }
