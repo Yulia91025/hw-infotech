7f65073245c27578d908ea6aea685d3b8918beb9
Юлия Худеньких <71973418+Yulia91025@users.noreply.github.com>
2021-09-19 05:00:34 +0300

    Assignments from first semester

---
+++ 1 семестр/6_2020.09.07/6_2020.09.07/6_2020.09.07.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+int f(int *m, int n) {
+	int k = 0;
+	cin >> n;
+	m = new int[n];
+
+	for (int i = 0; i < n; i++) {
+		cin >> m[i];
+	}
+
+	for (int i = 0; i < n; i++) {
+		if (m[i] % 2 == 0) k++;
+	}
+	int *temp = new int[n - k]; int t = 0;
+
+	for (int i = 0; i < n; i++) {
+		if (m[i] % 2 != 0)
+		{
+			temp[t] = m[i];
+			t++;
+		}
+	}
+	delete(m);
+	for (int i = 0; i < n - k; i++) {
+		cout << temp[i] << " ";
+	}
+}
+int main()
+{
+	int *m;
+	int n;
+	cin >> n;
+	f(m, n);
+}
